a26983565f0bfcfa7073bea27d2a8e6f39979f2f
Jinny <ejy921@gmail.com>
2024-10-01 03:34:53 -0500

    Update source.py

--- source.py
+++ source.py
@@ -1,14 +1,9 @@
 import json
 from sklearn.neighbors import NearestNeighbors
 import numpy as np
-<<<<<<< HEAD
 
 characteristics = [
     "Analytical", "Inquisitive", "Modern", "Pragmatic", "Empathetic", 
     "Skeptical", "Introspective", "Philosophical", "Visionary", "Creative", 
     "Spiritual", "Challenging", "Hopeful", "Uplifting"
 ]
-
-schools = []
-=======
->>>>>>> 7fa386bba165ebe4178d0d7dbd0e1c6874080e4b
