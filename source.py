c3a74a6cea49842bf227fb55741b2431f2080a49
Jinny <ejy921@gmail.com>
2024-10-01 03:33:43 -0500

    Update source.py

--- source.py
+++ source.py
@@ -1,6 +1,11 @@
-<<<<<<< Updated upstream
 import json
-=======
 from sklearn.neighbors import NearestNeighbors
 import numpy as np
->>>>>>> Stashed changes
+
+characteristics = [
+    "Analytical", "Inquisitive", "Modern", "Pragmatic", "Empathetic", 
+    "Skeptical", "Introspective", "Philosophical", "Visionary", "Creative", 
+    "Spiritual", "Challenging", "Hopeful", "Uplifting"
+]
+
+schools = []
