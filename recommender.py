740d8b55f0dc27f662dfc449e6e9d267f8b3d04b
ejy921 <ejy921@gmail.com>
2025-01-09 10:49:43 +0545

    update_profile for feedback implementation

--- recommender.py
+++ recommender.py
@@ -1,1 +1,39 @@
+import numpy as np
 from sklearn.neighbors import NearestNeighbors
+
+class RecommenderSystem:
+    def __init__(self, quote_dataset, user_profile):
+        self.quote_dataset = quote_dataset
+        self.user_profile = user_profile
+
+        # adjust as needed
+        self.knn_model = NearestNeighbors(n_neighbors=5, metric='cosine')
+        self.fit_model()
+
+    def fit_model(self):
+        feature_vectors = self.quote_dataset.feature_vectors
+        self.knn_model.fit(feature_vectors)
+        
+    # recommends based on user profile
+    def recommend(self, top_n=5):
+        # vectorized form, reshape to 2d array suited for knn model
+        user_vector = self.user_profile.profile_vector.reshape(1, -1)
+
+        # Get the top N similar quotes based on cosine similarity
+        distances, indices = self.knn_model.kneighbors(user_vector, n_neighbors=top_n)
+
+        recommendations = []
+        for i in indices[0]:
+            quote = self.quote_dataset[i]
+            recommendations.append((quote.quote_text, distances[0][i])) # include distance between user and quote for context
+        
+        return recommendations
+    
+    def update_with_feedback(self, quote_i, feedback):
+        # get feedback based on quote
+        quote_vector = self.quote_dataset.features_vectors[quote_i]
+        self.user_profile.update_profile(quote_vector, feedback)
+
+
+
+
--- user.py
+++ user.py
@@ -1,9 +1,19 @@
 import numpy as np
 
-class UserProfile:
-  def __init__(self, central_schools, default_preferences=None):
-    # self.central_schools = central_schools
-    self.pref_vector = np.zeros(len(central_schools))
+from features import Features 
 
-  def like(self, quote_features):
-    self.pref_vector += quote_features
+class UserProfile:
+  def __init__(self, initial_preferences=None):
+    self.profile_vector = np.zeros(len(Features.central_characteristics) + len(Features.central_philosophies)) if initial_preferences is None else initial_preferences
+
+  def get_profile(self):
+    return self.profile_vector
+
+  def update_profile(self, quote_vector, feedback_type):
+    if feedback_type == 'like':
+      self.profile_vector += quote_vector # increase influence of this quote's features
+    elif feedback_type == 'dislike':
+      self.profile_vector -= quote_vector
+
+    # Optional: Normalize the profile vector to prevent extreme values
+    self.profile_vector = np.clip(self.profile_vector, 0, 1)
